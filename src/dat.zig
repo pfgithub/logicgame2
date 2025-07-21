a34028af64de70913219b7e83740b93ab9ecf1fe
pfg <pfg@pfg.pw>
2025-07-21 12:48:10 -0700

    ,,?,

--- src/dat.zig
+++ src/dat.zig
@@ -8,3 +8,15 @@ const Component = struct {
 const PlacedComponent = struct {
     ul: @Vector(2, i32),
 };
+
+// basic components:
+// 1x wire, 2x wire, 4x wire, 8x wire, 16x wire, 32x wire, 64x wire
+// n-to-n splitters and mergers for these wires
+//    - it would be nice if we didn't need this?
+// bridges
+//    - this might not be its own component
+//    - alternatives:
+//      - o1: two layers where wires automatically make vias
+//      - o2: if you drag a wire across another wire it makes a long wire that doesn't connect
+// 1-1 npn "transistor" (or not gate, we choose)
+// all other components are user components
